5eb7194bfc771614289090aed4ed694a36bda631
Eric Hare <ericrhare@gmail.com>
2022-07-13 16:57:05 -0700

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -4,4 +4,15 @@ from transformers import pipeline
 sentiment_task = pipeline("sentiment-analysis", model="sentiment-roberta-large-english", tokenizer="sentiment-roberta-large-english")
 
 def get_sentiment(text):
+    '''
+    Use a Sentiment Detection model on the given text
+    
+    This function takes a string of text and returns the sentiment (POSITIVE/NEGATIVE)
+    along with the associated confidence / probability. 
+
+    :param str text: The text to derive the sentiment of
+    
+    :return: Results of the sentiment analysis algorithm
+    '''
+
     return sentiment_task(text)
