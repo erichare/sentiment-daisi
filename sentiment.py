2c281d51a9ba1f02b602beee297d62444a46f816
Eric Hare <ericrhare@gmail.com>
2022-07-13 16:46:13 -0700

    Add model config

--- sentiment.py
+++ sentiment.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from transformers import pipeline
 
-sentiment_task = pipeline("sentiment-analysis", model="model", tokenizer="model")
+sentiment_task = pipeline("sentiment-analysis", model="sentiment-roberta-large-english", tokenizer="sentiment-roberta-large-english")
 
 def get_sentiment(text):
     return sentiment_task(text)
